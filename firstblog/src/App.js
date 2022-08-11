16e9122f31b7c6343e460514945b4f10457d142d
amro abulaban <amro_abulaban@outlook.com>
2022-08-11 18:15:56 +0300

    first

---
+++ firstblog/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <header class="page-header">
+			<h1 class="entry-title">التصنيف: <span>Arabic Blog</span></h1>		</header>
+    </div>
+  );
+}
+
+export default App;
