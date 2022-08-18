70c357a80bbb01b8ae7dbb0962d60a2f5d61dcae
amro abulaban <amro_abulaban@outlook.com>
2022-08-18 16:46:28 +0300

    7

--- firstblog/src/App.jsx
+++ firstblog/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {BrowserRouter, Route} from "react-router-dom"
+import {BrowserRouter, Route, Switch} from "react-router-dom"
 import './App.css';
 import Navbar from "./components/Navbar";
 import AllPosts from "./components/AllPosts";
 import OnePost from "./components/OnePost";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -14,9 +14,11 @@ function App() {
 <BrowserRouter>
        <div>
         <Navbar/>
+        <Switch>
          <Route component={AllPosts}  exact path="/" /> 
          <Route component={OnePost} path="/onepost/:slug" /> 
-         
+         </Switch>
+         <Footer/>
       </div>
       
 
---
+++ firstblog/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Footer  () {
+  return (
+    <div>
+        footer
+    </div>
+    
+        )
+    }
+
+export default Footer;
